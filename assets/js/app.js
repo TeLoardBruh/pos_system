9e359ad9dd402e7a4982f7563dcad8acb1a47707
Rax <damnloardbruh@gmail.com>
2020-03-05 18:27:39 +0700

    interface fully done

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,14 +17,14 @@ var shoppingCart = (function() {
   
   // Save cart
   function saveCart() {
-    sessionStorage.setItem('shoppingCart', JSON.stringify(cart));
+    localStorage.setItem('shoppingCart', JSON.stringify(cart));
   }
   
     // Load cart
   function loadCart() {
-    cart = JSON.parse(sessionStorage.getItem('shoppingCart'));
+    cart = JSON.parse(localStorage.getItem('shoppingCart'));
   }
-  if (sessionStorage.getItem("shoppingCart") != null) {
+  if (localStorage.getItem("shoppingCart") != null) {
     loadCart();
   }
   
@@ -158,23 +158,47 @@ $('.clear-cart').click(function() {
 function displayCart() {
   var cartArray = shoppingCart.listCart();
   var output = "";
+  var userOutput = "";
   for(var i in cartArray) {
-    output += "<tr>"
-      + "<td>" + cartArray[i].name + "</td>" 
-      + "<td>(" + cartArray[i].price + ")</td>"
-      + "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name=" + cartArray[i].name + ">-</button>"
+    output += 
+    "<tr>"
+      +"<td></td>"
+      + "<td scope='col'>" + cartArray[i].name + "</td>" 
+      + "<td scope='col'><div class='input-group'>"
       + "<input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
-      + "<button class='plus-item btn btn-primary input-group-addon' data-name=" + cartArray[i].name + ">+</button></div></td>"
-      + "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>"
+      + "<button class='minus-item input-group-addon btn btn-warning' data-name=" + cartArray[i].name + ">-</button></div></td>"
+      + "<td scope='col'>" + cartArray[i].total + "</td>" 
+      + "<td scope='col'><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>"
       + " = " 
-      + "<td>" + cartArray[i].total + "</td>" 
+      +  "</tr>"
+      +"<div>Total price: $<span class='total-cart'></span></div>"
+      ;
+  }
+
+  for(var i in cartArray) {
+    userOutput += 
+    "<tr>"
+      +"<td></td>"
+      + "<td scope='col'>" + cartArray[i].name + "</td>" 
+      + "<td scope='col'>"+ cartArray[i].count + "</td>"
+      + "<td scope='col'>" + cartArray[i].total + "</td>" 
       +  "</tr>";
   }
+  $('.tableCenterContent').html(userOutput);
   $('.show-cart').html(output);
   $('.total-cart').html(shoppingCart.totalCart());
   $('.total-count').html(shoppingCart.totalCount());
 }
-
+// "<tr>"
+// + "<td scope='col'>" + cartArray[i].name + "</td>" 
+// + "<td scope='col'>" + cartArray[i].price + "</td>"
+// + "<td scope='col'><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name=" + cartArray[i].name + ">-</button>"
+// + "<input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
+// + "<button class='plus-item btn btn-primary input-group-addon' data-name=" + cartArray[i].name + ">+</button></div></td>"
+// + "<td scope='col'><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>"
+// + " = " 
+// + "<td scope='col'>" + cartArray[i].total + "</td>" 
+// +  "</tr>"
 // Delete item button
 
 $('.show-cart').on("click", ".delete-item", function(event) {
